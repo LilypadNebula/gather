8f042b139e3dede064466a674a48b1ddc8e47655
Lily <lilypadnebula@gmail.com>
2021-02-12 10:05:28 -0500

    Init

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<Router {routes} />
